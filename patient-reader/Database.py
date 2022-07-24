8a1f9b82c086f6ca17e81b0af7225b1ddce309ad
Cody Benkoski <cob322@lehigh.edu>
2022-07-24 14:54:01 -0400

    Iniital docker-compose.yaml, don't init db immediately

--- patient-reader/Database.py
+++ patient-reader/Database.py
@@ -1,12 +1,6 @@
 import mysql.connector
 from PatientModel import PatientModel
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="root",  
-) 
-
 class Database:
     def __init__(self):
         self.db = mysql.connector.connect(
