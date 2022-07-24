e6714102fd804c29fed4dcf38f4ce41ef24028bf
Cody Benkoski <cob322@lehigh.edu>
2022-07-24 13:35:16 -0400

    Add a patient class

---
+++ hospital-base/PatientModel.py
@@ -0,0 +1,6 @@
+class PatientModel:
+    def __init__(self, name:str, age:int, phone:str, patient_id:int):
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.id = patient_id
