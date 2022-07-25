0fe623882bb2733562dd43395462b9ec3d88e15b
Cody Benkoski <cob322@lehigh.edu>
2022-07-25 19:28:57 -0400

    Include connection settings in base image

--- hospital-base/constants.py
+++ hospital-base/constants.py
@@ -1,1 +1,12 @@
 GATEWAY_FQDN = "hospital-reader-gateway.local"
+DB_FQDN = "hospital-database.local"
+# yes, these should be environment variables,
+# but, since we're not deploying this on the internet we'll keep it like
+# this for simplicity
+DB_PASSWORD = "root"
+DB_USER = "root"
+MYSQL_CONNECT_OPTIONS = {
+    "host": DB_FQDN,
+    "user": "root",
+    "passwd": "root",
+}
